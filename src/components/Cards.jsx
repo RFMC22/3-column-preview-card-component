64df25877e5a9d3cca688f40722c40743ae151c2
Renee Magallanes <rene22797@gmail.com>
2023-07-15 01:01:03 -0500

    arreglando bug

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,13 @@
 const Cards = ({cars}) => {
+  const colors = {
+    'bright-orange': 'bg-bright-orange',
+    'dark-cyan': 'bg-dark-cyan',
+    'very-dark-cyan': 'bg-very-dark-cyan'
+  }
   return (
     <div className="card-container">
       {cars.map(car => (
-        <div key={car.title} className={`w-full bg-${car.color} h-full p-[48px]`}>
+        <div key={car.title} className={`w-full ${colors[car.color]} h-full p-[48px]`}>
           <img src={car.logo} alt={`logo ${car.title.toLowerCase()}`} />
           <h1 className="title">{car.title}</h1>
           <p className="text">{car.text}</p>
