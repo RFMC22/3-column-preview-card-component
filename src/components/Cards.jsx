082183b81429f51d659b2014a219601a97cb9b0c
Renee Magallanes <rene22797@gmail.com>
2023-07-16 02:23:56 -0500

    corrigiendo accesibilidad

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,10 +6,11 @@ const Cards = ({cars}) => {
   }
   return (
     <div className="card-container">
+      <h1 className="sr-only">3-column preview card component</h1>
       {cars.map(car => (
         <div key={car.title} className={`w-full ${colors[car.color]} h-full p-[48px]`}>
           <img src={car.logo} alt={`logo ${car.title.toLowerCase()}`} />
-          <h1 className="title">{car.title}</h1>
+          <h2 className="title">{car.title}</h2>
           <p className="text">{car.text}</p>
           <button className="btn btn-hover">Learn More</button>
         </div>
